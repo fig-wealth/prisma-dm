9e91f81d836ab16c5b228c3559aed00e0c57c880
Softjey <misivsvatoslav@gmail.com>
2025-01-07 21:19:20 +0100

    redesign cli to use dependency injection

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 
+import { getConfig } from "./config/getConfig";
 import { CLI } from "./services/CLI";
+import { Logger } from "./services/Logger";
+import { PrismaService } from "./services/PrismaService";
+import { ScriptRunner } from "./services/ScriptRunner";
+import { TargetedPrismaMigrator } from "./services/TargetedPrismaMigrator";
+import { Validator } from "./services/Validator";
 
 const args = process.argv.slice(2);
 
@@ -12,7 +18,19 @@ if (args.length === 0) {
   process.exit(1);
 }
 
-const cli = new CLI();
+if (args[0] === "init") {
+  CLI.init();
+
+  process.exit(0);
+}
+
+const config = getConfig();
+const logger = new Logger(config);
+const prisma = new PrismaService();
+const validator = new Validator(config);
+const scriptRunner = new ScriptRunner(config);
+const migrator = new TargetedPrismaMigrator(logger);
+const cli = new CLI(migrator, scriptRunner, prisma, validator, logger);
 const command = args[0];
 
 console.log(`Running command: ${command}`);
